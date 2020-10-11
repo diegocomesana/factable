e528b6fde3b72bd66be1d1f76779d40277fb9946
Diego Comesaña <dcomesana>
2020-10-10 23:15:01 -0300

    evidencer!!

--- src/evidencer/index.js
+++ src/evidencer/index.js
@@ -7,16 +7,39 @@ const resolvePath = (p) => path.resolve(__dirname, p);
 
 class FactableEvidencer {
   constructor(config) {
-    console.log("FactableEvidencer CONSTRUCTOR:", config);
+    console.log("FactableEvidencer CONSTRUCTOR fuckck:", config);
     this.pending = [];
     this.socket_ready = false;
+    this.socket = false;
+    this.reatemptTimeout = false;
 
+    this.initConnection();
+  }
+
+  initConnection() {
+    console.log("initConnection ");
+    if (this.reatemptTimeout) {
+      clearTimeout(this.reatemptTimeout);
+    }
     this.socket = new WebSocket("ws://localhost:8888");
+    this.socket.on("error", (err) => {
+      this.socket_ready = false;
+      console.error(
+        "Socket encountered error: ",
+        err.message,
+        "Closing socket"
+      );
+      this.socket.close();
+      this.reatemptTimeout = setTimeout(() => {
+        this.initConnection();
+      }, 1000);
+    });
     this.socket.on("open", this.heartbeat.bind(this));
-    this.socket.on("ping", this.heartbeat.bind(this));
+    // this.socket.on("ping", this.heartbeat.bind(this));
     this.socket.on("close", () => {
       this.socket_ready = false;
-      this.clearTimeout(this.pingTimeout);
+      // clearTimeout(this.pingTimeout);
+      this.initConnection();
     });
   }
 
@@ -24,6 +47,7 @@ class FactableEvidencer {
     if (this.socket_ready) {
       while (this.pending.length) {
         const current = this.pending.shift();
+        console.log("registerFunctionCall REAL");
         this.socket.send(
           safeJsonStringify(msgWrapper("registerFunctionCall", current))
         );
@@ -33,17 +57,9 @@ class FactableEvidencer {
 
   // https://github.com/websockets/ws#client-authentication
   heartbeat() {
+    console.log("heartbeat");
     this.socket_ready = true;
     this.processPending();
-    clearTimeout(this.pingTimeout);
-
-    // Use `WebSocket#terminate()`, which immediately destroys the connection,
-    // instead of `WebSocket#close()`, which waits for the close timer.
-    // Delay should be equal to the interval at which your server
-    // sends out pings plus a conservative assumption of the latency.
-    this.pingTimeout = setTimeout(() => {
-      // this.terminate();
-    }, 30000 + 1000);
   }
 
   registerFunctionCall(args, output, metadata) {
@@ -51,53 +67,10 @@ class FactableEvidencer {
       .createHash("md5")
       .update(safeJsonStringify(args))
       .digest("hex");
-    // console.log(`------------------------------------------------------`);
-    // console.log(`-> ${metadata.name}`);
-    // console.log(`---->${paramsHash}`);
-    // console.log(`---->${safeJsonStringify(args, 2)}`);
-    // console.log(`------------------------------------------------------`);
-    // console.log(`---->${safeJsonStringify(output, 2)}`);
-    // console.log(`------------------------------------------------------`);
-    // console.log(`--`);
-
-    // this.socket.send("Hello Server!");
-    console.log("registerFunctionCall");
-
+    // console.log("registerFunctionCall");
     const rand = Math.random().toString();
-
     this.pending.push({ args, output, metadata, paramsHash, rand });
     this.processPending();
-
-    /*
-    const fileTemplate = format(
-      `
-    
-    const givenInput = ${safeJsonStringify(args, 2)};
-
-    const expectedOutput = ${safeJsonStringify(output, 2)};
-    
-    
-    `,
-      { parser: "babel" }
-    );
-
-    const basePath = resolvePath(
-      `../../../../test-cases/${camelToDash(functionName)}/`
-    );
-
-    ensureDirExists(basePath)
-      .then((path) => {
-        // console.log("lalalal ---- ", path);
-        return createFile(
-          resolvePath(`${basePath}/${paramsHash}.js`),
-          fileTemplate
-        );
-      })
-      .then((path) => {
-        console.log(`----- ${functionName} test-case writen in ${path}`);
-      })
-      .catch((err) => console.log(err));
-      */
   }
 }
 
